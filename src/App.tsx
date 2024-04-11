c5eede27715cdf786e1c7bc3db214b8982ae694b
ivisheshrajput <ivisheshrajput@gmail.com>
2024-04-12 01:07:10 +0530

    Routing Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
+// import { BrowserRouter, Routes,Route } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import Topbar from "./pages/Topbar";
+import Sidebar from "./pages/Sidebar";
+import Dashboard from "./pages/Dashboard";
+
+import Error from "./pages/Error";
+import Shimmer from "./pages/Shimmer";
+const Product = lazy(() => import("./pages/Product"));
+const Customer = lazy(() => import("./pages/Customer"));
+const Transaction = lazy(() => import("./pages/Transaction"));
+
 const App = () => {
   return (
-    <div className="bg-red-500 ">App</div>
-  )
-}
+    <>
+      <Suspense fallback={<Shimmer />}>
+        <Topbar />
+        <Outlet />
+        <Sidebar />
+      </Suspense>
+    </>
+  );
+};
 
-export default App
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/product",
+        element: <Product />,
+      },
+      {
+        path: "/customer",
+        element: <Customer />,
+      },
+      {
+        path: "/transaction",
+        element: <Transaction />,
+      },
+    ],
+  },
+]);
+
+export default appRouter;
---
+++ src/pages/Shimmer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Shimmer = () => {
+  return (
+    <div className='text-2xl font-bold text-red-600 flex justify-center items-center '>Loading......</div>
+  )
+}
+
+export default Shimmer
